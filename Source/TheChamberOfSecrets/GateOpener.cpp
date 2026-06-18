3885cc1272f9f1fd43222c3920766e6f2488461d
Hawshil <12345manishjain@gmail.com>
2026-06-18 17:47:22 +0530

    Add compact location logging in TickComponent

--- Source/TheChamberOfSecrets/GateOpener.cpp
+++ Source/TheChamberOfSecrets/GateOpener.cpp
@@ -13,4 +13,14 @@ void UGateOpener::BeginPlay()
 void UGateOpener::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// AActor *CellGate = GetOwner();
+	// UE_LOG(LogTemp, Log, TEXT("CellGate address: %u"), CellGate);
+	// UE_LOG(LogTemp, Log, TEXT("CellGate name: %s"), *CellGate->GetName());
+
+	// FVector coords = CellGate->GetActorLocation();
+	// FString coordString = coords.ToCompactString();
+	// UE_LOG(LogTemp, Log, TEXT("Coordinates of the Cell Gate are: %s"), *coordString);
+
+	UE_LOG(LogTemp, Log, TEXT("Compact Logging: %s"), *((GetOwner()->GetActorLocation()).ToCompactString()));
 }
